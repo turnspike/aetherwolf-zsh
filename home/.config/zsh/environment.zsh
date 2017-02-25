1172e6e5ce64691710c89c491485b72c89d66b5b
turnspike <hello@turnspike.com>
2017-02-26 10:27:01 +1030

    fix zsh history

--- home/.config/zsh/environment.zsh
+++ home/.config/zsh/environment.zsh
@@ -1,18 +1,36 @@
 #!/bin/zsh
+
+#---- init
+ZCACHE=$XDG_CACHE_HOME/zsh
+[ -d $ZCACHE ] || mkdir $ZCACHE
+
+#---- general
 export BLOCKSIZE=1k # set default blocksize for ls, df, du
 
-## TODO: is this block still  needed on a modern zsh install?
-## ignore duplicate and cruft history items
-export HISTCONTROL="ignoreboth"
-export HISTIGNORE="&:ls:[bf]g:exit:l:ll:.."
-#shopt -s cmdhist # fix for multiline commands
+#---- history
 
+HISTFILE="$ZCACHE/.zsh_history"
+HISTSIZE=100000
+SAVEHIST=100000
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing nonexistent history.
+
+#---- input
 KEYTIMEOUT=1 # 10ms for key sequences
 
-setopt inc_append_history # Appends every command to the history file once it is executed
-setopt share_history # Reloads the history whenever you use it
-
-## locale settings for perl, ruby
+#---- locale
+# (used by perl, ruby etc)
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
--- home/.zshrc
+++ home/.zshrc
@@ -1,4 +1,11 @@
+export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 source $XDG_CONFIG_HOME/zsh/init.zsh
 
+# TODO: grok fzf
 #[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+if [[ `uname` == 'Darwin' ]]
+then
+	export PATH="/usr/local/sbin:$PATH"
+fi
