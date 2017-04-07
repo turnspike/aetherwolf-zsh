1528231f409724fb4b231dd128ea2a27f8529818
turnspike <hello@turnspike.com>
2017-04-07 20:47:55 +0930

    add zsh keybind to move forward/back word

--- home/.config/zsh/keybinds.zsh
+++ home/.config/zsh/keybinds.zsh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+# ---- zsh-history-substring-search
+bindkey '^[[A' history-substring-search-up # up key to search up
+bindkey '^[[B' history-substring-search-down # down key to search down
 
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
+
+# ---- zsh-autosuggestions
+bindkey '^[[1;5C' forward-word # ctrl-right accepts next word in cmd suggestion
+bindkey '^[[1;5D' backward-word # ctrl-left moves back one word
